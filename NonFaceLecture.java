f4a611cca07cba554f3f0679340f151cce0525f2
wjdwo1112 <104138263+wjdwo1112@users.noreply.github.com>
2023-07-26 22:38:23 +0900

    Baekjoon19532

---
+++ NonFaceLecture.java
@@ -0,0 +1,27 @@
+package Baik;
+
+import java.util.Scanner;
+
+
+public class NonFaceLecture {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = sc.nextInt();
+		int d = sc.nextInt();
+		int e = sc.nextInt();
+		int f = sc.nextInt();
+		
+		for(int i=-999; i<=999; i++) {
+			for(int j=-999; j<=999; j++) {
+			if(a*i+b*j==c && d*i+e*j==f) {
+				System.out.printf("%d %d",i,j);
+			}
+		}
+	}
+
+}
+}
